a9e8050fcc200f8857a07defca267266ba3b681c
ProskurinAK <andrew.prosto.andrew@gmail.com>
2023-12-22 23:22:12 +0300

    rework stdSet file

--- DataStructures/stdContainers/Set/stdSet.cpp
+++ DataStructures/stdContainers/Set/stdSet.cpp
@@ -3,63 +3,79 @@
 #include <unordered_set>
 using namespace std;
 
+// контейнер set реализует собой бинарное дерево. Бинарное дерево это упорядоченная структура данных с неповторяющимися элементами
+// контейнер multiset тоже реализует собой бинарное дерево, но в отличие от set элементы у него могут повторяться
+// контейнеры unordered_set и unordered_multiset являются неупорядоченными так как реализуют не бинарное дерево, а хэш-таблицу
+
 int main()
 {
 	srand(time(NULL));
 
-	// объявление и инициализация
-	set<int> testSet = { 9, 1, 4, 3, 5 , 3, 1, 7};
+	// ---------------------------------------- set ----------------------------------------
 
-	// вывод множества
+	//set<int> testSet = { 9, 1, 4, 3, 5, 3, 1, 7 };
+
+	// размер сета
+	//cout << testSet.size() << endl;
+
+	// функция ищет заданный элемент и возврощает итератор на него
+	//cout << *testSet.find(7) << endl;
+
+	// функция ищет заданный элемент и возврощает 0 или 1 в зависимости от результата поиска
+	//cout << testSet.count(7) << endl;
+
+	// добавление элемента в set
+	//testSet.insert(11);
+
+	// удаление элемента из множества
+	//testSet.erase(5);
+
+	// Вывод информации
+
 	/*for (int elem : testSet)
 	{
 		cout << elem << endl;
 	}*/
 
-	// размер множества
-	//cout << testSet.size() << endl;
+	// ---------------------------------------- multiset ----------------------------------------
 
-	// добавление элемента
-	//testSet.insert(10);
+	//multiset<int> testMultiset = { 9, 1, 4, 3, 5, 3, 1, 7 };
 
-	// удаление элемента
-	//testSet.erase(4);
+	// функция возврощает итератор на первый элемент(если таких несколько) найденный в контейнере
+	//cout << *testMultiset.lower_bound(3) << endl;
 
-	// проверка наличия элемента
-	//cout << testSet.count(0) << endl;	// функция вернёт 0
-	//cout << testSet.count(3) << endl;	// функция вернёт 1
+	// функция возврощает итератор на следующий элемент после последнего(если таких несколько) того что мы передали в функцию
+	//cout << *testMultiset.upper_bound(3) << endl;
 
-	// неупорядоченное множество
-	/*unordered_set<int> testUnorderedSet = { 7, 1, 4, 7, 8, 2 };
+	/*for (int elem : testMultiset)
+	{
+		cout << elem << endl;
+	}*/
 
-	testUnorderedSet.insert(3);
+	// ---------------------------------------- unordered_set ----------------------------------------
+
+	/*unordered_set<int> testUnorderedSet = { 9, 1, 4, 3, 5, 3, 1, 7 };
+	
+	testUnorderedSet.insert(2);
 	testUnorderedSet.insert(10);
+	testUnorderedSet.insert(8);
+	testUnorderedSet.insert(6);
 
 	for (int elem : testUnorderedSet)
 	{
-		cout << elem << "\t";
+		cout << elem << endl;
 	}*/
 
-	// упорядоченное мульти множество
-	/*multiset<int> testMultiset = { 7, 1, 4, 7, 8, 2 };
+	// ---------------------------------------- unordered_multiset ----------------------------------------
 
-	testMultiset.insert(3);
-	testMultiset.insert(10);
+	/*unordered_multiset<int> testUnorderedMultiset = { 9, 1, 4, 3, 5, 3, 1, 7 };
 
-	for (int elem : testMultiset)
-	{
-		cout << elem << "\t";
-	}*/
-
-	// неупорядоченное мульти множество
-	/*unordered_multiset<int> testUnorderedMultiset = { 7, 1, 4, 7, 8, 2 };
-
-	testUnorderedMultiset.insert(3);
+	testUnorderedMultiset.insert(2);
 	testUnorderedMultiset.insert(10);
 
 	for (int elem : testUnorderedMultiset)
 	{
-		cout << elem << "\t";
+		cout << elem << endl;
 	}*/
 
 	return 0;
